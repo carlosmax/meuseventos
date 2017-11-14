d74356c38285680a7f8d8eb1a19f547e58770524
Carlos Max <carlosmax.dev@hotmail.com>
2017-11-14 17:48:01 -0200

    Testes de integração

---
+++ server/tests/MeusEventos.Tests.Integration/Environment.cs
@@ -0,0 +1,27 @@
+﻿using MeusEventos.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.IO;
+using System.Net.Http;
+
+namespace MeusEventos.Tests.Integration
+{
+    public class Environment
+    {
+        public static TestServer Server { get; set; }
+        public static HttpClient Client { get; set; }
+
+        public static void CreateServer()
+        {
+            Server = new TestServer(
+                new WebHostBuilder()
+                    .UseKestrel()
+                    .UseEnvironment("Testing")
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseUrls("http://localhost:8285")
+                    .UseStartup<Startup>());
+
+            Client = Server.CreateClient();
+        }
+    }
+}
